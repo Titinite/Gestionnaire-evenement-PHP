6a4950489c8647a96015885bed5a2beefc47d0d6
Titinite <thibault.leray@supdevinci-edu.fr>
2024-11-08 14:38:13 +0100

    Update

--- add_event.php
+++ add_event.php
@@ -5,8 +5,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $title = $_POST['title'];
     $event_description = $_POST['event_description'];
     $event_date = $_POST['event_date'];
-    $created_date = new DateTime('Y-m-d H:i:s');
-    addEvent($title, $event_description, $event_date, $created_date);
+    $created_date = new DateTime();
+    addEvent($title, $event_description, $event_date, $created_date->format('Y-m-d H:i:s'));
 }
 
 header('Location: index.php');
