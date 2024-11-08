30e5e547af20d672d88adf1958a8fd76297f4f73
Kant1-18 <138484109+Kant1-18@users.noreply.github.com>
2024-11-08 14:32:58 +0100

    correctif date

--- add_event.php
+++ add_event.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $title = $_POST['title'];
     $event_description = $_POST['event_description'];
     $event_date = $_POST['event_date'];
-    $created_date = date('d/m H:i');
+    $created_date = new DateTime('Y-m-d H:i:s');
     addEvent($title, $event_description, $event_date, $created_date);
 }
 
